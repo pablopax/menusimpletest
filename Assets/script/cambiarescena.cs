54426b62409521350840b439044d0bbd03dd7ae8
pablopax <loscartografos@gmail.com>
2015-09-16 23:39:43 -0300

    MASTER 16092015 2339

---
+++ Assets/script/cambiarescena.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cambiarescena : MonoBehaviour {
+
+	//funcion escena
+	public void Cambiar(int indice)
+	{
+		Application.LoadLevel(indice);
+	}
+
+}
